0bb387b16329c42855de67049a267887053d28ff
fiona-gladwin <fionagladwin@multicorewareinc.com>
2022-07-25 01:30:46 -0400

    MInor fix

--- rocAL/rocAL/include/meta_data/coco_meta_data_reader.h
+++ rocAL/rocAL/include/meta_data/coco_meta_data_reader.h
@@ -36,7 +36,6 @@ public:
     void release(std::string image_name);
     void release() override;
     void print_map_contents();
-    std::map<std::string, std::shared_ptr<MetaData>> &get_map_content() override{ return _map_content;}
     MetaDataBatch * get_output() override { return _output; }
     const std::map<std::string, std::shared_ptr<MetaData>> & get_map_content() override { return _map_content;}
     COCOMetaDataReader();
