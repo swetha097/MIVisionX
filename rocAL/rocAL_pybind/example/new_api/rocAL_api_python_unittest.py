96ae6c32459d95be2b5a8c693e72f22eff6f3ddf
Swetha B S <swetha@multicorewareinc.com>
2022-08-18 05:48:13 -0400

    Adding Python support for Resize API

--- rocAL/rocAL_pybind/example/new_api/rocAL_api_python_unittest.py
+++ rocAL/rocAL_pybind/example/new_api/rocAL_api_python_unittest.py
@@ -55,13 +55,13 @@ def main():
     with pipe:
         jpegs, _ = fn.readers.file(file_root=data_path, shard_id=local_rank, num_shards=world_size, random_shuffle=True)
         images = fn.decoders.image(jpegs, file_root=data_path, device=decoder_device, output_type=types.RGB, shard_id=0, num_shards=1, random_shuffle=True)
-        images = fn.resize(images, device=rocal_device, resize_x=300, resize_y=300)
+        # images = fn.resize(images, device=rocal_device, resize_x=300, resize_y=300)
 
 
         if augmentation_name == "resize":
             output = fn.resize(images, device=rocal_device, resize_x=300, resize_y=300)
         if augmentation_name == "resize_shorter":
-            images = fn.resize_shorter(images, resize_size=256)
+            images = fn.resize(images,scaling_mode=types.SCALING_MODE_NOT_SMALLER, interpolation_type=types.TRIANGULAR_INTERPOLATION, resize_shorter=256)
             images = fn.centre_crop(images,crop=(224, 224))
             output = fn.crop_mirror_normalize(images, device="cpu",
                                               output_dtype=types.FLOAT16 if args.fp16 else types.FLOAT,
