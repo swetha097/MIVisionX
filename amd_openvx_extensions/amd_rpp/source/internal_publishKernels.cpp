40c6ea0cd57b960f33134189b3b9080662ed26da
fiona-gladwin <fionagladwin@multicorewareinc.com>
2022-09-22 02:51:20 -0400

    Minor fix - internal publish kernel names

--- amd_openvx_extensions/amd_rpp/source/internal_publishKernels.cpp
+++ amd_openvx_extensions/amd_rpp/source/internal_publishKernels.cpp
@@ -123,7 +123,7 @@ vx_status get_kernels_to_publish()
     STATUS_ERROR_CHECK(ADD_KERENEL(ResizeCropMirrorPD_Register));
     STATUS_ERROR_CHECK(ADD_KERENEL(CopybatchPD_Register));
     STATUS_ERROR_CHECK(ADD_KERENEL(NopbatchPD_Register));
-    STATUS_ERROR_CHECK(ADD_KERENEL(SequenceRearrange_Register));
+    STATUS_ERROR_CHECK(ADD_KERENEL(SequenceRearrangebatchPD_Register));
     STATUS_ERROR_CHECK(ADD_KERENEL(Brightness_Register));
     STATUS_ERROR_CHECK(ADD_KERENEL(GammaCorrection_Register));
     STATUS_ERROR_CHECK(ADD_KERENEL(CropMirrorNormalize_Register));
