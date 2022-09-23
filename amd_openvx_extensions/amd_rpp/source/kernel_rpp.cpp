a6a2b5a84592f5ec8a3dc1baf452897c5fa6c5d3
fiona-gladwin <fionagladwin@multicorewareinc.com>
2022-09-23 00:47:09 -0400

    CMN fix parameters

--- amd_openvx_extensions/amd_rpp/source/kernel_rpp.cpp
+++ amd_openvx_extensions/amd_rpp/source/kernel_rpp.cpp
@@ -1932,7 +1932,7 @@ VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_GammaCorrection(vx_graph graph, vx
 //     return node;
 // }
 
-VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_CropMirrorNormalize(vx_graph graph, vx_tensor pSrc, vx_array srcROI, vx_tensor pDst, vx_array dstROI,vx_array crop_w,vx_array crop_h, vx_array x1, vx_array y1, vx_array mean, vx_array std_dev, vx_array flip, vx_scalar is_packed, vx_scalar chnShift,vx_scalar layout, vx_scalar roiType, vx_uint32 nbatchSize)
+VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_CropMirrorNormalize(vx_graph graph, vx_tensor pSrc, vx_array srcROI, vx_tensor pDst, vx_array dstROI,vx_array crop_w,vx_array crop_h, vx_array x1, vx_array y1, vx_array mean, vx_array std_dev, vx_array flip, vx_scalar layout, vx_scalar roiType, vx_uint32 nbatchSize)
 {
     vx_node node = NULL;
     vx_context context = vxGetContext((vx_reference)graph);
@@ -1953,13 +1953,11 @@ VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_CropMirrorNormalize(vx_graph graph
             (vx_reference)mean,
             (vx_reference)std_dev,
             (vx_reference)flip,
-            (vx_reference)is_packed,
-            (vx_reference)chnShift,
             (vx_reference)layout,
             (vx_reference)roiType,
             (vx_reference)NBATCHSIZE,
             (vx_reference)DEV_TYPE};
-        node = createNode(graph, VX_KERNEL_RPP_CROPMIRRORNORMALIZE, params, 17);
+        node = createNode(graph, VX_KERNEL_RPP_CROPMIRRORNORMALIZE, params, 15);
     }
     return node;
 }
