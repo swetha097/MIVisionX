055a80d3b9c3d7c3e2e6c107f863c4bc1ae44651
fiona-gladwin <fionagladwin@multicorewareinc.com>
2022-11-29 14:49:04 -0500

    Minor build fix and changes

--- rocAL/rocAL/include/pipeline/tensor.h
+++ rocAL/rocAL/include/pipeline/tensor.h
@@ -93,7 +93,7 @@ public:
         } else {
             THROW("Invalid layout conversion")
         }   
-        for(int i = 0; i < _num_of_dims, i++)
+        for(int i = 0; i < _num_of_dims; i++)
             new_dims[i] = _dims.at(dims_mapping[i]);
     }
     void set_max_shape() {
