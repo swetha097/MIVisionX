18896bf74afb8fafdef87153fb2408a02c66925b
fiona-gladwin <fionagladwin@multicorewareinc.com>
2023-03-02 13:40:13 -0500

    Crop node file changes
    Single handle creation
    Pass crop coordinates as vx_tensor directly from node_crop.cpp
    Change query for HOST and HIP buffers

--- amd_openvx_extensions/amd_rpp/source/kernel_rpp.cpp
+++ amd_openvx_extensions/amd_rpp/source/kernel_rpp.cpp
@@ -1974,7 +1974,7 @@ VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_Copy(vx_graph graph, vx_tensor pSr
 }
 
 
-VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_Crop(vx_graph graph, vx_tensor pSrc, vx_array srcROI, vx_tensor pDst, vx_array dstROI,vx_array crop_w,vx_array crop_h, vx_array x1, vx_array y1, vx_scalar is_packed, vx_scalar chnShift,vx_scalar layout, vx_scalar roiType, vx_uint32 nbatchSize)
+VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_Crop(vx_graph graph, vx_tensor pSrc, vx_tensor srcROI, vx_tensor pDst, vx_scalar inputLayout, vx_scalar outputLayout, vx_scalar roiType)
 {
     vx_node node = NULL;
     vx_context context = vxGetContext((vx_reference)graph);
@@ -1982,23 +1982,15 @@ VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_Crop(vx_graph graph, vx_tensor pSr
     {
         vx_uint32 dev_type = getGraphAffinity(graph);
         vx_scalar DEV_TYPE = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_UINT32, &dev_type);
-        vx_scalar NBATCHSIZE = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_UINT32, &nbatchSize);
         vx_reference params[] = {
             (vx_reference)pSrc,
             (vx_reference)srcROI,
             (vx_reference)pDst,
-            (vx_reference)dstROI,
-            (vx_reference)crop_w,
-            (vx_reference)crop_h,
-            (vx_reference)x1,
-            (vx_reference)y1,
-            (vx_reference)is_packed,
-            (vx_reference)chnShift,
-            (vx_reference)layout,
+            (vx_reference)inputLayout,
+            (vx_reference)outputLayout,
             (vx_reference)roiType,
-            (vx_reference)NBATCHSIZE,
             (vx_reference)DEV_TYPE};
-        node = createNode(graph, VX_KERNEL_RPP_CROP, params, 14);
+        node = createNode(graph, VX_KERNEL_RPP_CROP, params, 7);
     }
     return node;
 }
