87580ba785e4584ebee6abe352bcfe61a2b79551
fiona-gladwin <fionagladwin@multicorewareinc.com>
2023-03-29 14:51:47 -0400

    Remove files related to Gamma
    Tensor was not integrated

--- amd_openvx_extensions/amd_rpp/source/kernel_rpp.cpp
+++ amd_openvx_extensions/amd_rpp/source/kernel_rpp.cpp
@@ -1898,28 +1898,6 @@ VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_Brightness(vx_graph graph, vx_tens
     return node;
 }
 
-VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_GammaCorrection(vx_graph graph, vx_tensor pSrc, vx_array srcImgWidth, vx_array srcImgHeight, vx_tensor pDst, vx_array gamma, vx_uint32 nbatchSize)
-{
-    vx_node node = NULL;
-    vx_context context = vxGetContext((vx_reference)graph);
-    if (vxGetStatus((vx_reference)context) == VX_SUCCESS)
-    {
-        vx_uint32 dev_type = getGraphAffinity(graph);
-        vx_scalar DEV_TYPE = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_UINT32, &dev_type);
-        vx_scalar NBATCHSIZE = vxCreateScalar(vxGetContext((vx_reference)graph), VX_TYPE_UINT32, &nbatchSize);
-        vx_reference params[] = {
-            (vx_reference)pSrc,
-            (vx_reference)srcImgWidth,
-            (vx_reference)srcImgHeight,
-            (vx_reference)pDst,
-            (vx_reference)gamma,
-            (vx_reference)NBATCHSIZE,
-            (vx_reference)DEV_TYPE};
-        node = createNode(graph, VX_KERNEL_RPP_GAMMACORRECTION, params, 7);
-    }
-    return node;
-}
-
 VX_API_ENTRY vx_node VX_API_CALL vxExtrppNode_CropMirrorNormalize(vx_graph graph, vx_tensor pSrc, vx_tensor srcROI, vx_tensor pDst, vx_array multiplier, vx_array offset, vx_array flip, vx_scalar inputLayout, vx_scalar outputLayout, vx_scalar roiType)
 {
     vx_node node = NULL;
