95b72c4213b3b93af70fb17232db97712aeca5c0
fiona-gladwin <fionagladwin@multicorewareinc.com>
2023-07-07 13:45:10 -0400

    Remove ToTensor API for f16 and f32
    Retaining the generic API

--- rocAL/rocAL/source/api/rocal_api_data_transfer.cpp
+++ rocAL/rocAL/source/api/rocal_api_data_transfer.cpp
@@ -28,54 +28,6 @@ THE SOFTWARE.
 #endif
 
 RocalStatus ROCAL_API_CALL
-rocalToTensor32(RocalContext p_context, float *out_ptr, RocalTensorLayout tensor_format, float multiplier0,
-                       float multiplier1, float multiplier2, float offset0, float offset1, float offset2,
-                       bool reverse_channels, RocalOutputMemType output_mem_type)
-{
-    auto context = static_cast<Context*>(p_context);
-    try
-    {
-        if(tensor_format != ROCAL_NHWC && tensor_format != ROCAL_NCHW)
-            THROW("Supported only for NHWC and NCHW tensor layout")
-
-        auto tensor_layout = (tensor_format == ROCAL_NHWC) ?  RocalTensorlayout::NHWC : RocalTensorlayout::NCHW;
-        context->master_graph->to_tensor(out_ptr, tensor_layout, multiplier0, multiplier1, multiplier2,
-                offset0, offset1, offset2, reverse_channels, RocalTensorDataType::FP32, output_mem_type);
-    }
-    catch(const std::exception& e)
-    {
-        context->capture_error(e.what());
-        ERR(e.what())
-        return ROCAL_RUNTIME_ERROR;
-    }
-    return ROCAL_OK;
-}
-
-RocalStatus ROCAL_API_CALL
-rocalToTensor16(RocalContext p_context, half *out_ptr, RocalTensorLayout tensor_format, float multiplier0,
-                       float multiplier1, float multiplier2, float offset0, float offset1, float offset2,
-                       bool reverse_channels, RocalOutputMemType output_mem_type)
-{
-    auto context = static_cast<Context*>(p_context);
-    try
-    {
-        if(tensor_format != ROCAL_NHWC && tensor_format != ROCAL_NCHW)
-            THROW("Supported only for NHWC and NCHW tensor layout")
-
-        auto tensor_layout = (tensor_format == ROCAL_NHWC) ?  RocalTensorlayout::NHWC : RocalTensorlayout::NCHW;
-        context->master_graph->to_tensor(out_ptr, tensor_layout, multiplier0, multiplier1, multiplier2,
-                offset0, offset1, offset2, reverse_channels, RocalTensorDataType::FP16, output_mem_type);
-    }
-    catch(const std::exception& e)
-    {
-        context->capture_error(e.what());
-        ERR(e.what())
-        return ROCAL_RUNTIME_ERROR;
-    }
-    return ROCAL_OK;
-}
-
-RocalStatus ROCAL_API_CALL
 rocalToTensor(RocalContext p_context, void *out_ptr, RocalTensorLayout tensor_format, RocalTensorOutputType tensor_output_type, float multiplier0,
                        float multiplier1, float multiplier2, float offset0, float offset1, float offset2,
                        bool reverse_channels, RocalOutputMemType output_mem_type)
@@ -85,6 +37,9 @@ rocalToTensor(RocalContext p_context, void *out_ptr, RocalTensorLayout tensor_fo
     {
         if(tensor_format != ROCAL_NHWC && tensor_format != ROCAL_NCHW)
             THROW("Supported only for NHWC and NCHW tensor layout")
+
+        if(tensor_output_type != ROCAL_FP32 && tensor_output_type != ROCAL_FP16)
+            THROW("Supported only for FP32 and FP16 tensor data types")
 
         auto tensor_layout = (tensor_format == ROCAL_NHWC) ?  RocalTensorlayout::NHWC : RocalTensorlayout::NCHW;
         auto tensor_output_data_type = (tensor_output_type == ROCAL_FP32) ? RocalTensorDataType::FP32 : RocalTensorDataType::FP16;
