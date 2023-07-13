19327c75217461bb29a21b96771623ef065e2f45
fiona-gladwin <fionagladwin@multicorewareinc.com>
2023-07-13 14:56:19 -0400

    Add changes in node files

--- rocAL/rocAL/include/pipeline/node.h
+++ rocAL/rocAL/include/pipeline/node.h
@@ -41,7 +41,7 @@ public:
     void add_next(const std::shared_ptr<Node>& node) {} // To be implemented
     void add_previous(const std::shared_ptr<Node>& node) {} //To be implemented
     std::shared_ptr<Graph> graph() { return _graph; }
-    void set_meta_data(pMetaDataBatch meta_data_info){ _meta_data_info = meta_data_info;}
+    void set_meta_data(pMetaDataBatch meta_data_info){ _meta_data_info = meta_data_info; }
     bool _is_ssd = false;
 protected:
     virtual void create_node() = 0;
@@ -54,5 +54,6 @@ protected:
     vx_node _node = nullptr;
     size_t _batch_size;
     pMetaDataBatch _meta_data_info;
+    vx_scalar _input_layout, _output_layout, _roi_type;
     vx_array _src_roi_width, _src_roi_height; // TODO - To be removed after tensor changes in augmentation nodes.
 };
