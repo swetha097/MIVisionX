4180677f3be63e57cf8d3a77bc4cc28703e13f2e
fiona-gladwin <fionagladwin@multicorewareinc.com>
2023-07-31 09:35:17 +0000

    Resolved PR comments for image_comparison script

--- utilities/rocAL/rocAL_unittests/pixel_comparison/image_comparison.py
+++ utilities/rocAL/rocAL_unittests/pixel_comparison/image_comparison.py
@@ -9,48 +9,38 @@ def compare_pixels(img1, img2, aug_name, width, height, image_offset = 0):
     if'rgb' in aug_name:
         pixels1 = img1.load()
         pixels2 = img2.load()
-        total_valid_pixel_count = width * height * 3
-        for wt in range(width):
-            for ht in range(height):
-                ht = ht + image_offset
-                if pixels1[wt,ht] != pixels2[wt,ht]:
-                    r = abs(pixels1[wt,ht][0]-pixels2[wt,ht][0])
-                    g = abs(pixels1[wt,ht][1]-pixels2[wt,ht][1])
-                    b = abs(pixels1[wt,ht][2]-pixels2[wt,ht][2])
-                    if(r > 4):
-                        r = 5
-                    if(g > 4):
-                        g = 5
-                    if(b > 4):
-                        b = 5
-                    pixel_difference[r] += 1
-                    pixel_difference[g] += 1
-                    pixel_difference[b] += 1
-                else:
-                    pixel_difference[0] += 3
+        channel=3
     else:
         pixels1 = img1.convert('L').load()
         pixels2 = img2.convert('L').load()
-        total_valid_pixel_count = width * height * 1
-        for wt in range(width):
-            for ht in range(height):
-                ht = ht + image_offset
-                if pixels1[wt,ht] != pixels2[wt,ht]:
-                    pixel = abs(pixels1[wt,ht]-pixels2[wt,ht])
-                    if(pixel > 4):
-                        pixel = 5
-                    pixel_difference[pixel] += 1
+        channel=1
+    total_valid_pixel_count = width * height * channel
+    for wt in range(width):
+        for ht in range(height):
+            ht = ht + image_offset
+            if pixels1[wt,ht] != pixels2[wt,ht]:
+                if(channel == 1 ):
+                    diff_val = abs(pixels1[wt,ht]-pixels2[wt,ht])
+                    diff_val = min(diff_val, 5)
+                    pixel_difference[diff_val] += 1
                 else:
-                    pixel_difference[0] += 1
+                    for ch in range(channel):
+                        diff_val = abs(pixels1[wt,ht][ch]-pixels2[wt,ht][ch])
+                        diff_val = min(diff_val, 5)
+                        pixel_difference[diff_val] += 1
+
+            else:
+                pixel_difference[0] += channel
     return pixel_difference, total_valid_pixel_count
 
 def main():
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S_")
     handlers = [logging.FileHandler('../rocal_unittest_log_file_' + timestamp + '.log'), logging.StreamHandler()]
     logging.basicConfig(level = logging.INFO, handlers = handlers)
-    if( len(sys.argv) < 3 ):
-        logging.error('Pass atleast 2 arguments (python image_comparison.py <ref_output_folder_path> <rocal_ouput_folder_path>')
-        exit()
+    if(len(sys.argv) < 3):
+        print ('Please pass ref_output_folder_path rocal_ouput_folder_path')
+        logging.error('Please pass ref_output_folder_path rocal_ouput_folder_path')
+        exit(0)
 
     # Open the two images
     ref_output_path = sys.argv[1]
@@ -70,24 +60,24 @@ def main():
     golden_file_path = ""
     for aug_name in rocal_output_dir_list:
         temp = aug_name.split('.')
-        file_name_s = temp[0].split('_')
-        if(len(file_name_s) > 3):
-            file_name_s.pop()
-            golden_file_path = "_".join(file_name_s) + ".png"
+        file_name_split = temp[0].split('_')
+        if(len(file_name_split) > 3):
+            file_name_split.pop()
+            golden_file_path = "_".join(file_name_split) + ".png"
         else:
             golden_file_path = aug_name
 
         #For randomized augmentation
-        if(file_name_s[0] in randomized_augmentation):
-            total_case_count=total_case_count+1
+        if(file_name_split[0] in randomized_augmentation):
+            total_case_count = total_case_count+1
             augmentation_name = aug_name.split('.')[0]
             logging.info("Running %s",augmentation_name)
-            passed_case_count=passed_case_count+1
+            passed_case_count=passed_case_count + 1
             logging.info("PASSED ")
         elif golden_file_path in golden_output_dir_list:
-            total_case_count=total_case_count+1
-            ref_file_path = ref_output_path+golden_file_path
-            rocal_file_path = rocal_output_path+aug_name
+            total_case_count = total_case_count+1
+            ref_file_path = ref_output_path + golden_file_path
+            rocal_file_path = rocal_output_path + aug_name
             if(os.path.exists(rocal_file_path) and os.path.exists(ref_file_path)):
                 logging.info("Running %s ",aug_name.split('.')[0])
                 img1 = Image.open(ref_file_path)
@@ -95,35 +85,35 @@ def main():
 
                 # Check if the images have the same dimensions
                 if img1.size != img2.size:
-                    logging.info("The images have different sizes.")
+                    logging.info("Golden output and augmentation outputs are having different sizes. Exiting!")
                     exit()
 
                 # Compare the pixel values for each image
-                pixeldiff = None
-                tot_count = 0
+                pixel_diff = None
+                total_count = 0
                 if 'larger' in aug_name:
                     resize_width = 400
                     resize_height = 300
                     image_offset = 400
-                    pixeldiff, tot_count = compare_pixels(img1, img2, aug_name, resize_width, resize_height)
-                    pixeldiff2, tot_count2 = compare_pixels(img1, img2, aug_name, resize_width, resize_height, image_offset)
-                    pixeldiff = [x + y for x, y in zip(pixeldiff, pixeldiff2)]
-                    tot_count = tot_count + tot_count2
+                    pixel_diff, total_count = compare_pixels(img1, img2, aug_name, resize_width, resize_height)
+                    pixel_diff2, total_count2 = compare_pixels(img1, img2, aug_name, resize_width, resize_height, image_offset)
+                    pixel_diff = [x + y for x, y in zip(pixel_diff, pixel_diff2)]
+                    total_count = total_count + total_count2
                 elif 'smaller' in aug_name:
                     resize_width = 533
                     resize_height = 400
                     image_offset = 2400
-                    pixeldiff, tot_count = compare_pixels(img1, img2, aug_name, resize_width, resize_height)
-                    pixeldiff2, tot_count2 = compare_pixels(img1, img2, aug_name, resize_width, resize_height, image_offset)
-                    pixeldiff = [x + y for x, y in zip(pixeldiff, pixeldiff2)]
-                    tot_count = tot_count + tot_count2
+                    pixel_diff, total_count = compare_pixels(img1, img2, aug_name, resize_width, resize_height)
+                    pixel_diff2, total_count2 = compare_pixels(img1, img2, aug_name, resize_width, resize_height, image_offset)
+                    pixel_diff = [x + y for x, y in zip(pixel_diff, pixel_diff2)]
+                    total_count = total_count + total_count2
                 else:
-                    pixeldiff, tot_count = compare_pixels(img1, img2, aug_name, img1.size[0], img1.size[1])
+                    pixel_diff, total_count = compare_pixels(img1, img2, aug_name, img1.size[0], img1.size[1])
                 total_pixel_diff = 0
                 for pix_diff in range(1,6):
-                    total_pixel_diff += pixeldiff[pix_diff]
-                mismatch_percentage = round((total_pixel_diff/tot_count)*100,2)
-                if(total_pixel_diff == 0 or (mismatch_percentage < 5.0 and pixeldiff[1] == total_pixel_diff) or          # Ignore test cases with single pixel differences less than 5% of total pixel count
+                    total_pixel_diff += pixel_diff[pix_diff]
+                mismatch_percentage = round((total_pixel_diff/total_count)*100,2)
+                if(total_pixel_diff == 0 or (mismatch_percentage < 5.0 and pixel_diff[1] == total_pixel_diff) or          # Ignore test cases with single pixel differences less than 5% of total pixel count
                    (mismatch_percentage < 0.5 and ('Blend' in aug_name or 'Rotate' in aug_name) and 'hip' in aug_name)): # Ignore mismatch in rotate augmentation less than 0.5% of total pixel count
                     passed_case_count=passed_case_count+1
                     logging.info("PASSED")
@@ -131,10 +121,10 @@ def main():
                     failed_case_list.append(golden_file_path)
                     failed_case_count=failed_case_count+1
                     logging.info("FAILED")
-                    logging.info("Printing pixel mismatch %s",pixeldiff)
+                    logging.info("Printing pixel mismatch %s",pixel_diff)
                     logging.info("Mismatach percentage %0.2f", mismatch_percentage)
                     for pix_diff in range(1,6):
-                            logging.info("Percentage of %d pixel mismatch %0.2f", pix_diff, round((pixeldiff[pix_diff]/total_pixel_diff)*100,2))
+                            logging.info("Percentage of %d pixel mismatch %0.2f", pix_diff, round((pixel_diff[pix_diff]/total_pixel_diff)*100,2))
             else:
                 logging.info("Skipping the testcase as file not found %s",rocal_file_path)
         else:
