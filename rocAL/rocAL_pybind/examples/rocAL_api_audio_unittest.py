e8b430ea3545f7d68c387318c13a469195b0229d
Swetha B S <swetha@multicorewareinc.com>
2023-08-03 10:39:01 +0000

    Python Support for PreEmphasis Filter

--- rocAL/rocAL_pybind/examples/rocAL_api_audio_unittest.py
+++ rocAL/rocAL_pybind/examples/rocAL_api_audio_unittest.py
@@ -56,27 +56,28 @@ def main():
             file_list=file_list,
             )
         audio_decode = fn.decoders.audio(audio, file_root=data_path, file_list_path=file_list, downmix=False, shard_id=0, num_shards=2, storage_type=9, stick_to_shard=False)
-        begin, length = fn.nonsilent_region(audio_decode, cutoff_db=-60)
-        trim_silence = fn.slice(
-            audio_decode,
-            anchor=[begin],
-            shape=[length],
-            normalized_anchor=False,
-            normalized_shape=False,
-            axes=[0],
-            rocal_tensor_output_type = types.FLOAT)
-        spec = fn.spectrogram(
-            audio_decode,
-            nfft=512,
-            window_length=320,
-            window_step=160,
-            rocal_tensor_output_type = types.FLOAT)
-        mel = fn.mel_filter_bank(
-            spec,
-            sample_rate=16000,
-            nfilter=80,
-        )
-        audio_pipeline.setOutputs(mel)
+        pre_emphasis_filter = fn.preemphasis_filter(audio_decode)
+        # begin, length = fn.nonsilent_region(audio_decode, cutoff_db=-60)
+        # trim_silence = fn.slice(
+        #     audio_decode,
+        #     anchor=[begin],
+        #     shape=[length],
+        #     normalized_anchor=False,
+        #     normalized_shape=False,
+        #     axes=[0],
+        #     rocal_tensor_output_type = types.FLOAT)
+        # spec = fn.spectrogram(
+        #     audio_decode,
+        #     nfft=512,
+        #     window_length=320,
+        #     window_step=160,
+        #     rocal_tensor_output_type = types.FLOAT)
+        # mel = fn.mel_filter_bank(
+        #     spec,
+        #     sample_rate=16000,
+        #     nfilter=80,
+        # )
+        audio_pipeline.setOutputs(pre_emphasis_filter)
     audio_pipeline.build()
     audioIteratorPipeline = ROCALClassificationIterator(audio_pipeline, auto_reset=True)
     cnt = 0
