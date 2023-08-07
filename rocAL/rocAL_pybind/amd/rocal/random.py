2cd13f7c3420936ab5f572a1d98b533d06823479
Swetha B S <swetha@multicorewareinc.com>
2023-08-07 11:21:04 +0000

    Code clean up in random.py

--- rocAL/rocAL_pybind/amd/rocal/random.py
+++ rocAL/rocAL_pybind/amd/rocal/random.py
@@ -29,12 +29,11 @@ def coin_flip(*inputs, probability=0.5):
     return output_array
 
 def uniform(*inputs, range=[-1.0, 1.0]):
-    kwargs_pybind = { "inputs":inputs[0],"is_output": False, "range":range }
+    kwargs_pybind = { "inputs":inputs[0], "is_output": False, "range":range }
     output_uniform_distribution = b.UniformDistribution(Pipeline._current_pipeline._handle ,*(kwargs_pybind.values()))
-    print("output_uniform_distribution",output_uniform_distribution)
     return (output_uniform_distribution)
 
-def normal(*inputs, mean=0.0, stddev=1.0, dtype=types.FLOAT):
-    kwargs_pybind = { "inputs":inputs[0],"is_output": False, "mean": mean, "stddev": stddev }
+def normal(*inputs, mean=0.0, stddev=1.0):
+    kwargs_pybind = { "inputs":inputs[0], "is_output": False, "mean": mean, "stddev": stddev }
     output_normal_distribution = b.NormalDistribution(Pipeline._current_pipeline._handle ,*(kwargs_pybind.values()))
     return (output_normal_distribution)
